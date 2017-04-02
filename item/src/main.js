7ee1b74297e6a8542695e610db9713b49b211da8
fxy5869571 <996276475@qq.com>
2017-04-02 12:15:15 +0800

    First commit

---
+++ item/src/main.js
@@ -0,0 +1,36 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+import App from './App';
+import goods from './components/goods/goods.vue';
+import ratings from './components/ratings/ratings.vue';
+import seller from './components/seller/seller.vue';
+
+import './common/stylus/index.styl';
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+let app = Vue.extend(App);
+let router = new VueRouter({
+  linkActiveClass: 'active'
+});
+
+router.map({
+  '/goods': {
+    component: goods
+  }
+});
+router.map({
+  '/ratings': {
+    component: ratings
+  }
+});
+router.map({
+  '/seller': {
+    component: seller
+  }
+});
+
+router.start(app, '#app');
+router.go('/goods');
